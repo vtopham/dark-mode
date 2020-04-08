74fe3094d209983a4dd816571b4ac2d44c743b0a
Victoria  Topham <topham.victoria@gmail.com>
2020-04-08 10:33:35 -0700

    filled out useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,14 @@
 
 
-export const useLocalStorage = (key, value) => {
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key)
+        return item ? JSON.parse(item) : initialValue
+    });
     
-
-    return;
+    const setValue = value => {
+        setStoredValue(value)
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
+    return [storedValue, setValue];
 }
